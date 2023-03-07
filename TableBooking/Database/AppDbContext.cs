43866702805916b7606cb75e1e4e228cd38fc8b6
bestronaut <nesimibesim@outlook.com>
2023-03-07 09:30:58 +0100

    Initial commit

---
+++ TableBooking/Database/AppDbContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TableBooking.Models;
+
+namespace TableBooking.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TableModel> Tables { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TableModel>().HasData(new TableModel()
+            {
+                Id = 1,
+                NumberOfSeats = 4,
+                TableNumber = 100,
+                IsBooked = false,
+            }, new TableModel()
+            {
+                Id = 2,
+                NumberOfSeats = 6,
+                TableNumber = 101,
+                IsBooked = false,
+            },
+            new TableModel()
+            {
+                Id = 3,
+                NumberOfSeats = 8,
+                TableNumber = 102,
+                IsBooked = false,
+
+            }, new TableModel()
+            {
+                Id = 4,
+                NumberOfSeats = 4,
+                TableNumber = 103,
+                IsBooked = false,
+
+            }, new TableModel()
+            {
+                Id = 5,
+                NumberOfSeats = 4,
+                TableNumber = 104,
+                IsBooked = false,
+
+            }, new TableModel
+            {
+                Id = 6,
+                NumberOfSeats = 8,
+                TableNumber = 105,
+                IsBooked = false,
+            });
+        }
+    }
+}
---
+++ TableBooking/Models/TableModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TableBooking.Models
+{
+    public class TableModel
+    {
+        public int Id { get; set; }
+        public int NumberOfSeats { get; set; }
+
+        public int TableNumber { get; set; }
+
+        public bool IsBooked { get; set; }
+    }
+}
